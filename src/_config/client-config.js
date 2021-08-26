dcdb262fbe72347be0a34fc9c73d64be63b23b51
katheermizal <katheermizal@gmail.com>
2021-08-26 12:17:22 +0530

    Media Files

---
+++ src/_config/client-config.js
@@ -0,0 +1,5 @@
+export const GoogleMapsAPI = 'YOUR_API_KEY';
+export const icons = {
+    propertyLocationMarker: '../assets/icons/map-icon.png',
+    myCurrentLocationMarker: '../assets/my-location-icon.png'
+}
